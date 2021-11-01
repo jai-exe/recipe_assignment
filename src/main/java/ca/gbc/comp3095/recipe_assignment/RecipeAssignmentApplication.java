eff7f4a0a4e824abdc9c268ea91a28860ab8559e
Jai Kumar <kjai1903@gmail.com>
2021-11-02 02:42:41 +0530

    Initial commit

---
+++ src/main/java/ca/gbc/comp3095/recipe_assignment/RecipeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.recipe_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/gbc/comp3095/recipe_assignment/RecipeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.recipe_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
